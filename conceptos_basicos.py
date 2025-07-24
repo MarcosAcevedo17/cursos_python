5186e4b76bd8886cb3719f7253a28aa652ce81fb
Marcos Acevedo Antonio <acevedomarcosantonio54@gmail.com>
2025-07-24 03:15:12 -0300

    sintaxis de Python

---
+++ conceptos_basicos.py
@@ -0,0 +1,39 @@
+print("INDENTACIÓN")
+print("En Python, la indentación (espacios o tabulaciones al inicio de una línea) se utiliza para delimitar bloques de código.")
+print("A diferencia de otros lenguajes que utilizan llaves o palabras clave, Python utiliza la indentación para determinar el alcance de las declaraciones. Por ejemplo:")
+
+print("""
+if condition:
+    # Bloque de código si la condición es verdadera
+    instruccion1
+    instruccion2
+else:
+    # Bloque de código si la condición es falsa
+    instruccion3
+    instruccion4
+""")
+
+print("====================================")
+
+print("COMENTARIOS")
+print("Los comentarios son líneas de texto en el código que son ignoradas por el intérprete de Python. Se utilizan para explicar o documentar el código.")
+print("En Python, los comentarios de una sola línea comienzan con el símbolo #, mientras que los comentarios de varias líneas se encierran entre triples comillas. Por ejemplo:")
+
+print("# Este es un comentario de una sola línea")
+
+print('''"""
+Este es un comentario
+de varias líneas
+"""''')
+
+print("====================================")
+
+print("SENSIBILIDAD A MAYÚSCULAS Y MINÚSCULAS")
+print("Python distingue entre mayúsculas y minúsculas. Por lo tanto, variable, Variable y VARIABLE se consideran variables diferentes.")
+
+print("====================================")
+
+print("PUNTO Y COMA")
+print("A diferencia de otros lenguajes, Python no requiere el uso de punto y coma (;) al final de cada instrucción.")
+print("Sin embargo, si deseas escribir varias instrucciones en una sola línea, puedes separarlas con un punto y coma. Por ejemplo:")
+print("instruccion1; instruccion2; instruccion3")
