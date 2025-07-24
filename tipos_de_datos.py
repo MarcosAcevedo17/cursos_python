4b849038b3a84bbaa281437a63e6d6203e9813e0
Marcos Acevedo Antonio <acevedomarcosantonio54@gmail.com>
2025-07-24 03:16:53 -0300

    tipos de datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,57 @@
+print("TIPOS DE DATOS BÁSICOS")
+print("""
+En Python, los tipos de datos básicos son las categorías en las que se pueden clasificar los valores que utilizamos en nuestros programas.
+Comprender los diferentes tipos de datos es fundamental para trabajar con variables y realizar operaciones en Python.
+Los tipos de datos básicos incluyen:
+""")
+
+print("1. ENTEROS (int)")
+print("""
+Los números enteros son aquellos que no tienen parte decimal.
+En Python, se representan simplemente escribiendo el número sin comillas ni puntos decimales. Por ejemplo:
+""")
+
+print("""
+edad = 25
+cantidad = 100
+""")
+
+print("2. FLOTANTES (float)")
+print("""
+Los números flotantes, también conocidos como números de punto flotante, son aquellos que tienen una parte decimal.
+En Python, se representan utilizando un punto para separar la parte entera de la parte decimal. Por ejemplo:
+""")
+
+print("""
+precio = 9.99
+altura = 1.75
+""")
+
+print("3. CADENAS DE TEXTO (strings)")
+print("""
+Las cadenas de texto, o simplemente cadenas, son secuencias de caracteres encerradas entre comillas simples ('...') o dobles ("...").
+Se utilizan para representar texto en Python. Por ejemplo:
+""")
+
+print("""
+nombre = "Juan"
+mensaje = '¡Hola, mundo!'
+""")
+
+print("""
+Puedes incluir caracteres especiales en las cadenas utilizando el carácter de escape \\.
+Por ejemplo, para incluir comillas dentro de una cadena, puedes usar \\' o \\".
+También puedes utilizar la notación de triple comilla ('''...''' o \"\"\"...\"\"\") para crear cadenas de varias líneas.
+""")
+
+print("4. BOOLEANOS (bool)")
+print("""
+Los valores booleanos representan los valores de verdad: True (verdadero) y False (falso).
+Se utilizan comúnmente en expresiones condicionales y operaciones lógicas. Por ejemplo:
+""")
+
+print("""
+es_mayor_de_edad = True
+tiene_descuento = False
+""")
+
